fe2a5acfe8c49f4c9c8819287345ab3e87f0e6f2
MicheleBenaco <benaco.michele@yahoo.it>
2022-12-20 17:53:54 +0100

    style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,36 @@
 <h1>
-    Sono header 
+    <header>
+        {{-- strisca azzura --}}
+        <div class="d-flex justify-content-end container-blue">
+    
+                <h5>DC POWERS </h5>
+            </div>
+            {{-- container nav-bar --}}
+        <div class="container d-flex justify-content-between align-items-center pt-4 ">
+            <div>
+                <img src="{{Vite::asset('/resources/img/dc-logo.png')}}" alt="">
+            </div>
+            <div class="nav-bar">
+                <ul class="d-flex justify-content-center ">
+                    <li>
+                       <a href="">blog</a>
+                    </li>
+                    <li>
+                        <a href="">Contatti</a>
+                    </li>
+                    <li>
+                        <a href="">Films</a>
+                    </li>
+                </ul>
+    
+    
+            </div>
+        </div>
+            {{-- JUMBOTRON --}}
+        <div class="jumbotron">
+            <img src="{{Vite::asset('/resources/img/jumbotron.jpg')}}" alt="">
+    
+        </div>
+    
+    </header>
 </h1>
